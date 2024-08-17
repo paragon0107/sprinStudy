3439f0807fb1d4bf0c4996822b5a0d1a77a9b67e
smg0725 <smg0725@naver.com>
2024-08-17 15:22:02 +0900

    .

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new  Member(username,age);
+        memberRepository.save(member);
+
+        ModelView modelView = new ModelView("save-result");
+        modelView.getModel().put("member", member);
+        return modelView;
+    }
+}
